376d66a1dceac11fa9baba86936b7043feaaf278
dimitrytar22 <dtarasenko212006@gmail.com>
2025-01-05 17:20:00 +0200

    final version

--- app/Http/Resources/Api/V1/FilmResource.php
+++ app/Http/Resources/Api/V1/FilmResource.php
@@ -4,7 +4,6 @@ namespace App\Http\Resources\Api\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use Illuminate\Support\Facades\Config;
 
 class FilmResource extends JsonResource
 {
@@ -20,7 +19,7 @@ class FilmResource extends JsonResource
             'title' => $this->title,
             'published' => $this->published,
             'genres' => GenreResource::collection($this->genres),
-            'poster_link' => url('/'). asset('images/'.$this->poster_link)
+            'poster_link' => url('/') . asset('images/'.$this->poster_link)
         ];
     }
 }
